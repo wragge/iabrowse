c5319d87f5d8e2be8812b53e15ed26831d7c34bf
Tim Sherratt <tim@discontents.com.au>
2015-06-24 21:35:39 +1000

    Add key navigation

--- static/js/script.js
+++ static/js/script.js
@@ -9,4 +9,21 @@ $(function() {
     $images.imagesLoaded().progress( function() {
         $images.isotope('layout');
     });
+    $("body").keydown(function(e){
+    // left arrow
+    if (e.which == 37) {
+        var previous = $("#previous").attr('href');
+        if (typeof previous !== "undefined") {
+            window.location.href = previous;
+        } 
+
+    // right arrow
+    } if (e.which == 39) {
+        // do something
+        var next = $("#next").attr('href');
+        if (typeof next !== "undefined") {
+            window.location.href = next;
+        } 
+    }   
+});
 });
